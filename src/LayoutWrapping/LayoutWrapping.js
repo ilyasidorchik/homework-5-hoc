fcc5ca4f3989ede7dc8fb13a6025f549a151430a
Ilya Sidorchik <ilya@sidorchik.ru>
2019-08-10 11:44:21 +0300

    Adds Wrapping by HOC

--- src/LayoutWrapping/LayoutWrapping.js
+++ src/LayoutWrapping/LayoutWrapping.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 
-/*
-  Напишите HOC, который обернёт компонент в `div`,
-  со стилем 'position: absolute'
-*/
-
-export const wrapWithAbsolutePosition = () => {}
+export const wrapWithAbsolutePosition = (WrappedComponent) => {
+  return class extends Component {
+    render() {
+      return (
+        <div style={{position: 'absolute'}}>
+          <WrappedComponent />
+        </div>
+      )
+    }
+  }
+};
